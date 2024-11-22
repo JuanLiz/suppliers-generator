dc6d1b96b70c5deb7bb1c789d97b207c66dec883
JuanLiz <47019693+JuanLiz@users.noreply.github.com>
2024-11-22 15:02:43 -0500

    feat(lists): Search and add products now working

--- app/lists/[id]/page.tsx
+++ app/lists/[id]/page.tsx
@@ -10,22 +10,33 @@ import {
     ProColumns,
     RequestData
 } from '@ant-design/pro-components';
-import { Plus, Search } from '@icon-park/react';
-import { Button, ConfigProvider, Input, Select, Space } from 'antd';
+import { PayCodeTwo, Plus, Search } from '@icon-park/react';
+import { Button, ConfigProvider, Input, message, Select, SelectProps, Space } from 'antd';
 import axios from 'axios';
-import { useEffect, useRef, useState } from "react";
+import debounce from 'lodash/debounce';
+import { useEffect, useMemo, useRef, useState } from "react";
+
+// For debounce search
+export interface DebounceSelectProps<ValueType = any>
+    extends Omit<SelectProps<ValueType | ValueType[]>, 'options' | 'children'> {
+    fetchOptions: (search: string) => Promise<ValueType[]>;
+    debounceTimeout?: number;
+}
 
 
 export default function ListPage(props: { params: { id: string; } }) {
 
+    //=== States and refs===//
+
     const [supplierList, setSupplierList] = useState<SupplierList>();
 
     // Search bar
-    const [open, setOpen] = useState(false);
-    const [options, setOptions] = useState<Product[]>([]);
-    const [selectedProduct, setSelectedProduct] = useState<Product>();
+    const searchRef = useRef<HTMLInputElement>(null);
+    const [selectedProduct, setSelectedProduct] = useState<ProductValue>();
+    const [selectedQuantity, setSelectedQuantity] = useState<number>();
     // const [value, setValue] = useState<UserValue[]>([]);
 
+    const [messageApi, contextHolder] = message.useMessage();
 
     //=== ProTable stuff ===//
     const [dataSource, setDataSource] = useState<readonly ListItem[]>([]);
@@ -43,28 +54,125 @@ export default function ListPage(props: { params: { id: string; } }) {
 
     }
 
-    async function getProductList(search: string) {
-        var isString = isNaN(Number(search));
-        if (isString) {
-            setTimeout(async () => {
-                var searchQuery = `${isString ? 'name' : 'sku'}=${search}`;
+    //=== Methods ===//
 
-                try {
-                    const response = await axios.get(`/api/products?${searchQuery}`);
-                    setOptions(isString
-                        // Add key to option based on id
-                        ? response.data.map((product: Product) => {
-                            return ({ ...product, key: product.id })
-                        })
-                        : [{ ...response.data, key: response.data.id }])
-                    console.log(response.data);
-                } catch (error) {
-                    console.error(error);
-                }
-            }, isString ? 1000 : 500);
-        }
 
+    //=== Search bar methods ===//
+    interface ProductValue {
+        label: string;
+        value: string;
+        title: string[];
     }
+
+    function DebounceSelect<
+        ValueType extends { key?: string; label: React.ReactNode; value: string | number } = any,
+    >({ fetchOptions, debounceTimeout = 800, ...props }: DebounceSelectProps<ValueType>) {
+        const [fetching, setFetching] = useState(false);
+        const [options, setOptions] = useState<ValueType[]>([]);
+        const fetchRef = useRef(0);
+
+        const debounceFetcher = useMemo(() => {
+            const loadOptions = (value: string) => {
+                console.log('fetching', value);
+                fetchRef.current += 1;
+                const fetchId = fetchRef.current;
+                setOptions([]);
+                setFetching(true);
+
+                // Return on empty search
+                if (value.length < 1) return;
+
+                var isString = isNaN(Number(value));
+
+                debounceTimeout = isString ? 800 : 20;
+
+                fetchOptions(value).then((newOptions) => {
+                    if (fetchId !== fetchRef.current) {
+                        // for fetch callback order
+                        return;
+                    }
+
+                    setOptions(newOptions);
+                    setFetching(false);
+                });
+            };
+
+            return debounce(loadOptions, debounceTimeout);
+        }, [fetchOptions, debounceTimeout]);
+
+        return (
+            <Select
+                labelInValue
+                filterOption={false}
+                onSearch={debounceFetcher}
+                {...props}
+                options={options}
+            />
+        );
+    }
+
+    async function getProductList(search: string): Promise<ProductValue[]> {
+        var isString = isNaN(Number(search));
+        var searchQuery = `${isString ? 'name' : 'sku'}=${search}`;
+
+        try {
+            const response = await axios.get(`/api/products?${searchQuery}`);
+            return isString ? response.data.map((product: Product) => {
+                return {
+                    label: (
+                        <div className="flex flex-col gap-1 leading-none">
+                            <div className='flex gap-1 items-center'>
+                                <PayCodeTwo theme="outline" size="12" fill="#9ca3af" />
+                                <span className="text-gray-400 text-xs">
+                                    {product.sku} - {product.supplier?.name}
+                                </span>
+                            </div>
+                            <span>{product.name}</span>
+                        </div>
+                    ),
+                    value: product.id,
+                    // Using title for store SKU and supplier name for search
+                    title: [product.sku, product.name, product.supplier?.name],
+                }
+            }) : [{
+                label: (<div className="flex flex-col gap-1 leading-none">
+                    <div className='flex gap-1 items-center'>
+                        <PayCodeTwo theme="outline" size="12" fill="#9ca3af" />
+                        <span className="text-gray-400 text-xs">
+                            {response.data.sku} - {response.data.supplier?.name}
+                        </span>
+                    </div>
+                    <span>{response.data.name}</span>
+                </div>),
+                value: response.data.id,
+                // Using title for store SKU and supplier name for search
+                title: [response.data.sku, response.data.name, response.data.supplier?.name],
+            }]
+        } catch (error) {
+            console.error(error);
+            return [];
+        }
+    }
+
+    async function createListItem(e: any) {
+        try {
+            const response = await axios.post('/api/lists/' + props.params.id + '/items', {
+                supplierListId: props.params.id,
+                productId: selectedProduct?.value,
+                quantity: selectedQuantity,
+            });
+            messageApi.success('Producto agregado correctamente');
+            actionRef.current?.reload();
+            setSelectedProduct(undefined);
+            setSelectedQuantity(undefined);
+        } catch (error) {
+            console.error(error);
+            messageApi.error('Error al agregar el producto');
+        }
+    }
+
+
+    //=== ProTable ===//
 
     async function getListItems(params: ParamsType): Promise<Partial<RequestData<ListItem>>> {
         // params.size = params.pageSize;
@@ -86,6 +194,16 @@ export default function ListPage(props: { params: { id: string; } }) {
             editable: false,
             render: (text, record, index, action) => {
                 return <span className='font-bold'>{record.product.sku}</span>
+            }
+        },
+        {
+            title: 'Cantidad',
+            dataIndex: 'quantity',
+            width: '5%',
+            editable: false,
+            align: 'right',
+            render: (text, record, index, action) => {
+                return <span className='font-bold text-end'>{record.quantity}</span>
             }
         },
         {
@@ -187,13 +305,39 @@ export default function ListPage(props: { params: { id: string; } }) {
     ];
 
 
+    //=== UseEffects ===//
+
     useEffect(() => {
         getSupplierList();
     }, []);
 
+    useEffect(() => {
+        const element = document.getElementById('search');
+        if (!supplierList || !dataSource || !element) return;
+        element.focus({});
+    }, [supplierList, dataSource]);
+
+    // Focus to quantity and set default to 1 on selected product
+    useEffect(() => {
+        if (!selectedProduct) return;
+        setSelectedQuantity(1);
+        document.getElementById('search')?.blur();
+    }, [selectedProduct]);
+
+    useEffect(() => {
+        if (!selectedQuantity) return;
+        const element = document.getElementById('quantity') as HTMLInputElement;
+        if (!element || document.activeElement == element) return;
+        element.focus({});
+        element.select();
+
+    }, [selectedQuantity]);
+
+
 
     return (
         <div className="bg-gray-200/70 w-screen min-h-screen">
+            {contextHolder}
             <main className="flex flex-col max-w-screen-xl mx-auto p-4 lg:p-12 gap-6">
                 {/* Search card */}
                 <div className="p-8 rounded-xl bg-white flex flex-col gap-4">
@@ -205,43 +349,59 @@ export default function ListPage(props: { params: { id: string; } }) {
                         }}
                     >
                         {supplierList && <h1 className="text-2xl font-bold">{supplierList.name}</h1>}
-                        <div className="flex items-center gap-4 flex-1">
+
+                        <div className='flex items-center gap-4 flex-1'>
                             <Space.Compact size="large" className='w-full'>
-                                <Select
+                                <DebounceSelect
                                     className='shadow-sm rounded-xl w-full'
                                     size='large'
+                                    id='search'
                                     style={{
                                         width: '90%',
                                         height: '3.5rem'
                                     }}
                                     suffixIcon={null}
-                                    prefix={<Search theme="outline" size="24" fill="#0B1215" />}
+                                    prefix={
+                                        <div className='pe-2'>
+                                            <Search theme="outline" size="24" fill="#0B1215" />
+                                        </div>
+                                    }
                                     placeholder="Escanea, escribe el código de barras o busca por nombre"
                                     showSearch
-                                // onChange={handleChange}
-                                // options={options}
+                                    notFoundContent={null}
+                                    fetchOptions={getProductList}
+                                    value={selectedProduct}
+                                    onChange={(value) => {
+                                        setSelectedProduct(value as ProductValue);
+                                    }}
                                 />
                                 <Input
+                                    type='number'
+                                    id='quantity'
                                     style={{
                                         width: '10%',
                                         height: '3.5rem'
                                     }}
-                                    type='number'
                                     min={1}
-                                    className=''
+                                    value={selectedQuantity}
+                                    placeholder='Cant.'
+                                    onChange={(e) => setSelectedQuantity(Number(e.target.value))}
+                                    onFocus={(e) => e.target.select()}
                                 />
                             </Space.Compact>
                             <div className='flex gap-0 items-center flex-1'>
                             </div>
                             <Button
                                 style={{
-                                    width: '3.75rem',
+                                    width: '3.79rem',
                                     height: '3.5rem'
                                 }}
                                 type="primary"
                                 size='large'
                                 shape="circle"
                                 icon={<Plus theme="outline" size="24" fill="#ffffff" />}
+                                disabled={!selectedProduct || !selectedQuantity}
+                                onClick={createListItem}
                             />
                         </div>
                     </ConfigProvider>
